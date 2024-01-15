cc3bcc3863411759b8daf1d5aa05bf131891a4cb
Bergqvist <anders.bergqvist@ihcg.se>
2024-01-15 18:42:47 +0100

    Funkar, dock hackigt

--- cliffwalking-policy-iteration.py
+++ cliffwalking-policy-iteration.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
 import numpy as np
 
+np.set_printoptions(linewidth=np.inf)
 env = gym.make('CliffWalking-v0')
 
 env.reset()
@@ -10,14 +11,14 @@ for i in range(37):
     states.append(0)
 
 for i in range(37, 47):
-    states.append(-100)
-states.append(100)
-#print(states)
+    states.append(1.11111) #Just to make print even
+states.append(0)
 
 gamma = 0.9
 
 do_not_caluclate = list(range(37, 48))
 for e in range(100):
+    threshold = 0
     for s in range(len(states)):       
         is_in_range = do_not_caluclate.count(s)
         if (is_in_range == 0): 
@@ -26,39 +27,35 @@ for e in range(100):
                 current_value = states[s]
                 s_prime = env.unwrapped.P[s][a][0][1]
                 r = env.unwrapped.P[s][a][0][2]
-                if (r == -100 and (a == 2 or a == 3)):
-                    continue
+                if (r == -100): #Without this the agent gets trapped in left corner
+                    r = -1 
                 expected_value += 0.25 * (r + gamma * states[s_prime])
-                # else:
-                #     states[s] = s_prime
-            states[s] = round(expected_value, 2) #round(current_value - expected_value, 3)
+            current_value
+                
+            states[s] = round(expected_value, 5)
 
-# for (i, item) in enumerate(states):
-#     print(f"{i}: {round(item, 3)}")
 
-np.set_printoptions(linewidth=np.inf)
 print(np.reshape(states, (4,12)))
+# För att utvärdra policyn
+done = False
 
-# # För att utvärdra policyn
-# done = False
+env = gym.make('CliffWalking-v0', render_mode="human")
 
-# env = gym.make('CliffWalking-v0', render_mode="human")
+observation = env.reset()
+observed_state = observation[0]
 
-# observation = env.reset()
-# observed_state = observation[0]
-
-# while done != True:
-#     env.render()
+while done != True:
+    env.render()
     
-#     action = 0
-#     current_value = states[observed_state]
-#     for a in (range(4)):
-#         s_prime = env.unwrapped.P[observed_state][a][0][1]
-#         r = env.unwrapped.P[observed_state][a][0][2]
-#         if (states[s_prime] > current_value):
-#             action = a
-#             current_value = states[s_prime]
-#     observation, reward, done, truncated, info = env.step(action)
-#     observed_state = observation
+    action = 0
+    current_value = states[observed_state]
+    for a in (range(4)):
+        s_prime = env.unwrapped.P[observed_state][a][0][1]
+        r = env.unwrapped.P[observed_state][a][0][2]
+        if (states[s_prime] > current_value):
+            action = a
+            current_value = states[s_prime]
+    observation, reward, done, truncated, info = env.step(action)
+    observed_state = observation
 
-# env.close()
+env.close()
